d0bd2a22e820a2d091f4ef4d7208665f14035565
babul101 <babuldeb101@gmail.com>
2024-02-13 16:05:15 +0530

    using state hook in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-
-import NotificationList from './components/NotificationList';
-import Notification from './interfaces/notification';
-
-const notifications: Notification[] = [
-  {id:1,title:'Notification 1'}
-]
+import { useState } from "react";
+import NotificationList from "./components/NotificationList";
+import Notification from "./interfaces/notification";
 
 function App() {
+  const [notifications, setNotifications] = useState<Notification[]>([
+    { id: 1, title: "Notification 1" },
+  ]);
   return (
-    <div className="App">
+    <div className='App'>
       <NotificationList items={notifications}></NotificationList>
     </div>
   );
